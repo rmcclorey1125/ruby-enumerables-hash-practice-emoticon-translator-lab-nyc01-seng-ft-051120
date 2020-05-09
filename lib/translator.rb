58f9e0fd77949bba4fc2f66063eea351808b3473
breezy-desk-5482 <rmcclorey1@gmail.com>
2020-05-09 02:09:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,31 +9,33 @@ def load_library(file_path)
    "get_emoticon" => {}
  }
   
+emoticons.each do |meaning, translations|
+  return_hash["get_meaning"][translations[1]] = meaning
+  return_hash["get_emoticon"][translations[0]] = translations[1]
+end
+return_hash
 end
 
 def get_japanese_emoticon(file_path, emoticon)
-  result = load_library(file_path)
-  emo = result["get_emotion"][emoticon]
-  if emo
-    emo
-  else
-    "Sorry,that emoticon was not found"
+  emoticons = load_library(file_path)
+  emoticons["get_emoticon"].each do |english_emoticon, japanese_emoticon|
+    if emoticon == english_emoticon
+      return japanese_emoticon
+    end
   end
-
+  return "Sorry, that emoticon was not found"
   # code goes here
 
 end
 
 def get_english_meaning(file_path, emoticon)
-  
-  result = load_library(file_path)
-  emo = result["get_meaning"][emoticon]
-  if emo
-    emo
-  else
-    "Sorry,that emoticon was not found"
+  emoticons = load_library(file_path)
+  emoticons["get_meaning"].each do |japanese_emoticon, english_meaning|
+    if emoticon == japanese_emoticon
+      return japanese_emoticon
+    end
   end
-  
+  return "Sorry, that emoticon was not found"
   # code goes here
   
 end
